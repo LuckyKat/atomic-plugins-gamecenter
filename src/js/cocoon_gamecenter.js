2a163d3925cf800b71d178adcc5ba45e9ed8696b
Marta Aguilera <marta@ludei.com>
2015-05-29 17:46:08 +0200

    Game Center doc updated

--- src/js/cocoon_gamecenter.js
+++ src/js/cocoon_gamecenter.js
@@ -6,15 +6,7 @@
     var Cocoon = window.Cocoon;
 
     /**
-     * This namespace represents all the basic functionalities available in the CocoonJS extension API.
-     *
-     * <div class="alert alert-success">
-     *   Here you will find a demo about this namespace: <a href="https://github.com/ludei/cocoonjs-demos/tree/Multiplayer">Multiplayer demo</a>.
-     *</div>
-     *
-     * <div class="alert alert-warning">
-     *    <strong>Warning!</strong> This JavaScript extension requires some configuration parameters on the <a href="https://ludei.zendesk.com/hc/en-us">cloud compiler</a>!
-     * </div>
+     * Cocoon Social Interface for the Game Center Extension.
      * @namespace Cocoon.Social.GameCenter
      */
     Cocoon.define("Cocoon.Social", function(extension) {
@@ -41,13 +33,6 @@
                     me.onGameCenterLoginStateChanged.emit("loginStatusChanged", [player]);
                 });
             },
-
-            /**
-             * This namespace represents all the basic functionalities available in the CocoonJS extension API.
-             * <div class="alert alert-warning">
-             *    <strong>Warning!</strong> This JavaScript extension requires some configuration parameters on the <a href="https://ludei.zendesk.com/hc/en-us">cloud compiler</a>!.
-             * </div>
-             */
 
             /**
              * Returns a Cocoon Social Interface for the Game Center Extension.
